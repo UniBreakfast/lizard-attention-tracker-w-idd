336bfbeb69034a971dc36a25e5d5acd54e4afb9f
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2020-04-25 10:38:04 +0300

    add poa list example

---
+++ poa-list-ex.js
@@ -0,0 +1,23 @@
+// poa - points of attention, aPrefer - attention preferable
+const poaList = [
+  {
+    name: "programming",
+    aPrefer: 7,
+    aActual: 4,
+  },
+  {
+    name: "knees",
+    aPrefer: 4,
+    aActual: 1,
+  },
+  {
+    name: "games",
+    aPrefer: 1,
+    aActual: 7,
+  },
+  {
+    name: "unhealthy food",
+    aPrefer: 1,
+    aActual: 6,
+  },
+]
