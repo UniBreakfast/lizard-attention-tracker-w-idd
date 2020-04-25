d7d7342592e24bb5ba17637b0f805abdca51138c
unibreakfast <unibreakfast1@gmail.com>
2020-04-25 19:47:54 +0300

    add coloring and number selection

--- script.js
+++ script.js
@@ -24,7 +24,29 @@ table.querySelectorAll('td:first-child').forEach(td =>
 function replaceWithNumScale(el, side='right') {
   el.value = el.innerText
   const nums = [...Array(10).keys()]
-  if (side == 'left') nums.reverse()
+  if (side == 'left') {
+    nums.reverse()
+    el.classList.add('left')
+  } else el.classList.add('right')
   el.innerHTML = nums.map(num => `<span>${num+1}</span>`).join('')
+
+  const cells = [...el.children]
   el.classList.add('num-scale')
+  el.contentEditable = 'false'
+
+  el.onmousemove =({target})=> {
+    if (el == target) return
+    cells.forEach(cell => {
+      if (+cell.innerText > +target.innerText)
+        cell.classList.remove('colored')
+      else cell.classList.add('colored')
+      cell.classList.remove('with-num')
+    })
+    target.classList.add('with-num')
+  }
+
+  el.onmouseleave =()=> el.onmousemove({target: [...el.children]
+    .find(cell => cell.innerText == el.value)})
+
+  el.onclick =({target})=> el.value = target.innerText
 }
