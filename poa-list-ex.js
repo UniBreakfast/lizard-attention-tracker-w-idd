2a646e0b3c49ea40b3ad3c23c2a87557f91846b0
unibreakfast <unibreakfast1@gmail.com>
2020-04-27 06:42:42 +0300

    simplify poaList example

--- poa-list-ex.js
+++ poa-list-ex.js
@@ -1,23 +1,7 @@
 // poa - points of attention, aPrefer - attention preferable
 const poaList = [
-  {
-    name: "programming",
-    aPrefer: 7,
-    aActual: 4,
-  },
-  {
-    name: "knees",
-    aPrefer: 4,
-    aActual: 1,
-  },
-  {
-    name: "games",
-    aPrefer: 1,
-    aActual: 7,
-  },
-  {
-    name: "unhealthy food",
-    aPrefer: 1,
-    aActual: 6,
-  },
+  [7, "programming", 4],
+  [4, "knees", 1],
+  [1, "games", 7],
+  [1, "unhealthy food", 6]
 ]
